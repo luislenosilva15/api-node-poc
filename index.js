c412d1f3c8c7334976cd94d21c15b0eb56f79f5f
Luis Silva <luis.silva@bemobi.com>
2026-01-22 14:43:31 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(cors());
 app.use(express.json());
 
 /**
